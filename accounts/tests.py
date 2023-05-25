7d3f803780c085449583bdf59f7f1b0d3b143a59
Saeed <133646450+SaeedRahimisp@users.noreply.github.com>
2023-05-25 21:04:10 +0330

    add test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class SignUpPageTest(TestCase):
+    username = 'new_username'
+    email = 'myusername@gmail.com'
+
+    def test_sign_up_urls_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_sign_up_urls(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_sign_up_form(self):
+        user = get_user_model().objects.create_user(
+            self.username,
+            self.email,
+        )
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
