1e335c9aec5fba2bdd493d7c7a64fb6fc47bcebc
Saeed <133646450+SaeedRahimisp@users.noreply.github.com>
2023-05-29 19:24:17 +0330

    Add cover image

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
-from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -10,4 +10,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
